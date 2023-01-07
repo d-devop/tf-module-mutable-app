75ec15f36258443bbdcf969e47a7e2a8674cd23c
Divya <divya5guntaka@gmail.com>
2023-01-07 09:37:49 +0530

    Update

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,9 @@
 resource "aws_spot_instance_request" "rabbitmq" {
+  count                  = var.instance_count
   ami                    = data.aws_ami.centos8.id
   instance_type          = var.instance_type
-  subnet_id              = local.db_subnets_ids[0]
-  vpc_security_group_ids = [aws_security_group.allow_rabbitmq.id]
+  subnet_id              = element(local.app_subnets_ids, count.index)
+  vpc_security_group_ids = [aws_security_group.allow_app.id]
   wait_for_fulfillment   = true
 
   tags = {
@@ -17,7 +18,8 @@ EOF
 }
 
 resource "aws_ec2_tag" "name-tag" {
-  resource_id = aws_spot_instance_request.rabbitmq.spot_instance_id
+  count                  = var.instance_count
+  resource_id = element(aws_spot_instance_request.rabbitmq.*.spot_instance_id, count.index)
   key         = "Name"
   value       = "${var.env}-rabbitmq"
 }
