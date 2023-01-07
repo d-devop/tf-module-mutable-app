592251dcbb79c6c618be731eec5da805eb0fdc5e
Divya <divya5guntaka@gmail.com>
2023-01-07 09:17:40 +0530

    Update

---
+++ vars.tf
@@ -0,0 +1,9 @@
+variable "env" {}
+variable "instance_type" {}
+variable "vpc" {}
+variable "component" {}
+variable "allow_ssh_cidr" {}
+
+variable "desired_capacity" {}
+variable "max_size" {}
+variable "min_size" {}
