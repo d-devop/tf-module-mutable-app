4c04acecbc0acc13603f48f4a63d7cb258c9ad94
Divya <divya5guntaka@gmail.com>
2023-01-10 13:17:06 +0530

    Update

--- locals.tf
+++ locals.tf
@@ -1,6 +1,10 @@
 locals {
-  vpc_id           = lookup(lookup(var.vpc, var.env, null), "vpc_id", null)
-  app_subnets_cidr = lookup(lookup(var.vpc, var.env, null), "app_subnets_cidr", null)
-  app_subnets_ids  = lookup(lookup(var.vpc, var.env, null), "app_subnets_ids", null)
+  vpc_id              = lookup(lookup(var.vpc, var.env, null), "vpc_id", null)
+  app_subnets_cidr    = lookup(lookup(var.vpc, var.env, null), "app_subnets_cidr", null)
+  public_subnets_cidr = lookup(lookup(var.vpc, var.env, null), "public_subnets_cidr", null)
+  app_subnets_ids     = lookup(lookup(var.vpc, var.env, null), "app_subnets_ids", null)
+  public_subnets_ids  = lookup(lookup(var.vpc, var.env, null), "public_subnets_ids", null)
+
+  allow_app_access = var.component == "frontend" ? local.public_subnets_cidr : local.app_subnets_cidr
 
 }
