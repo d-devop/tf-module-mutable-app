a5c797793d53eaaa01e14ee218ba8f9ddfb52acf
Divya <divya5guntaka@gmail.com>
2023-01-10 18:17:38 +0530

    Update

--- iam.tf
+++ iam.tf
@@ -4,31 +4,31 @@ resource "aws_iam_policy" "ssm-policy" {
   description = "${var.env}-ssm-to-ec2-${var.component}"
 
   policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
   {
-    "Version": "2012-10-17",
-    "Statement": [
-  {
-    "Sid": "VisualEditor0",
-    "Effect": "Allow",
-    "Action": [
+    "Sid" : "VisualEditor0",
+    "Effect" : "Allow",
+    "Action" : [
     "ssm:GetParameterHistory",
     "ssm:GetParametersByPath",
     "ssm:GetParameters",
     "ssm:GetParameter"
   ],
-    "Resource": [
-      "arn:aws:ssm:us-east-1:136168207246:parameter/param.dev*",
-      "arn:aws:ssm:us-east-1:136168207246:parameter/param.NEXUS*"
-    ]
+    "Resource" : [
+    "arn:aws:ssm:us-east-1:136168207246:parameter/param.dev*",
+    "arn:aws:ssm:us-east-1:136168207246:parameter/param.NEXUS*"
+  ]
   },
   {
-    "Sid": "VisualEditor1",
-    "Effect": "Allow",
-    "Action": "ssm:DescribeParameters",
-    "Resource": "*"
-  }
-  ]
-  }
+    "Sid" : "VisualEditor1",
+    "Effect" : "Allow",
+    "Action" : "ssm:DescribeParameters",
+    "Resource" : "*"
+      }
+    ]
+  })
+}
 
 resource "aws_iam_role" "ec2-role" {
   name = "${var.env}-ec2-${var.component}"
