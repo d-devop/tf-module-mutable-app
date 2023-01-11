f48e4a87e704b13f495a2443db7197413df048ca
Divya <divya5guntaka@gmail.com>
2023-01-11 11:29:49 +0530

    Update

--- locals.tf
+++ locals.tf
@@ -7,4 +7,10 @@ locals {
 
   allow_app_access = var.component == "frontend" ? local.public_subnets_cidr : local.app_subnets_cidr
 
+
+  alb_arn = { for k, v in var.load_balancers : k => lookup(v.alb, "arn", null) }
+  arn     = var.component == "frontend" ? local.alb_arn["public"] : local.alb_arn["private"]
+
+  alb_dns_name = { for k, v in var.load_balancers : k => lookup(v.alb, "dns_name", null) }
+  dns_name     = var.component == "frontend" ? local.alb_dns_name["public"] : local.alb_dns_name["private"]
 }
