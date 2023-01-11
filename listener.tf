89f8a7e81dd7d1ff7f0cfa6814fd63257294aac6
Divya <divya5guntaka@gmail.com>
2023-01-11 10:48:31 +0530

    Update

---
+++ listener.tf
@@ -0,0 +1,46 @@
+resource "aws_lb_listener" "listener_public" {
+  count             = var.component == "frontend" ? 1 : 0
+  load_balancer_arn = local.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
+
+resource "aws_lb_listener" "listener_private" {
+  count             = var.component == "frontend" ? 0 : 1
+  load_balancer_arn = local.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "404"
+    }
+  }
+}
+
+resource "aws_lb_listener_rule" "static" {
+  count        = var.component == "frontend" ? 0 : 1
+  listener_arn = aws_lb_listener.listener_private.*.arn[0]
+  priority     = var.lb_rule_priority
+
+  action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+
+
+  condition {
+    host_header {
+      values = ["${var.env}-${var.component}.${var.domain}"]
+    }
+  }
+}
