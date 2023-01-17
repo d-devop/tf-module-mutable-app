09dc8a70ac2cc5bc901fdeb2d2383c953b2c5c29
Divya <divya5guntaka@gmail.com>
2023-01-17 10:44:47 +0530

    Update

--- iam.tf
+++ iam.tf
@@ -6,25 +6,25 @@ resource "aws_iam_policy" "ssm-policy" {
   policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
-  {
-    "Sid" : "VisualEditor0",
-    "Effect" : "Allow",
-    "Action" : [
-    "ssm:GetParameterHistory",
-    "ssm:GetParametersByPath",
-    "ssm:GetParameters",
-    "ssm:GetParameter"
-  ],
-    "Resource" : [
-    "arn:aws:ssm:us-east-1:136168207246:parameter/param.dev*",
-    "arn:aws:ssm:us-east-1:136168207246:parameter/param.NEXUS*"
-  ]
-  },
-  {
-    "Sid" : "VisualEditor1",
-    "Effect" : "Allow",
-    "Action" : "ssm:DescribeParameters",
-    "Resource" : "*"
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm:GetParameterHistory",
+          "ssm:GetParametersByPath",
+          "ssm:GetParameters",
+          "ssm:GetParameter"
+        ],
+        "Resource" : [
+          "arn:aws:ssm:us-east-1:136168207246:parameter/param.${var.env}*",
+          "arn:aws:ssm:us-east-1:136168207246:parameter/param.NEXUS*"
+        ]
+      },
+      {
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : "ssm:DescribeParameters",
+        "Resource" : "*"
       }
     ]
   })
